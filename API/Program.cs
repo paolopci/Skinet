ac1932833072f39e729c52eb483b253a8432b591
Paolo Paci <paolopci@yahoo.it>
2026-01-13 17:14:15 +0100

    Initial implementation GenericRepository

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<StoreContext>(options =>
 
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 
 
 builder.Services.AddControllers();
---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T>(StoreContext context):IGenericRepository<T> where T: Core.Entities.BaseEntity
+    {
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await context
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
