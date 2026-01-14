6a38fc430aac6fe03bf4ef30e9efe2faac98eefd
Paolo Paci <paolopci@yahoo.it>
2026-01-14 16:18:17 +0100

    add SpecificationEvaluator

--- Core/Specification/BaseSpecification.cs
+++ Core/Specification/BaseSpecification.cs
@@ -4,14 +4,7 @@ using Core.Interfaces;
 
 namespace Core.Specification;
 
-public class BaseSpecification<T> : ISpecification<T>
+public class BaseSpecification<T>(Expression<Func<T, bool>> criteria) : ISpecification<T>
 {
-    private readonly Expression<Func<T, bool>> _criteria;
-
-    public BaseSpecification(Expression<Func<T, bool>> criteria)
-    {
-        this._criteria = criteria;
-    }
-
-    public Expression<Func<T, bool>> Criteria => _criteria;
+    public Expression<Func<T, bool>> Criteria => criteria;
 }
