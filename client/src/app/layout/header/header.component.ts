5fc6e09e51b444823ab1c2de1ecb73a20d3905a3
Paolo Paci <paolopci@yahoo.it>
2026-01-21 11:21:52 +0100

    add material.ts and update header.component.html

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
-import { MatButton } from '@angular/material/button';
-import { MatBadge } from '@angular/material/badge';
+import { MatBadge, MatButton, MatIcon } from '../../shared/material';
 
 @Component({
   selector: 'app-header',
