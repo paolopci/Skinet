ae481a4876a209523f7570c2ad052c7e93e8921d
Paolo Paci <paolopci@yahoo.it>
2026-01-23 16:50:42 +0100

    fix material.ts

--- client/src/app/shared/material.ts
+++ client/src/app/shared/material.ts
@@ -2,5 +2,15 @@ import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatCard } from '@angular/material/card';
+import { MatCardActions, MatCardContent } from '@angular/material/card';
 
-export { MatBadge, MatButton, MatIcon, MatCard };
+// prettier-ignore
+const MATERIAL_IMPORTS = [
+    MatBadge, 
+    MatButton, 
+    MatIcon, 
+    MatCard, 
+    MatCardContent, 
+    MatCardActions
+];
+export { MATERIAL_IMPORTS };
