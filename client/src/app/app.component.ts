b92226fd8de57f28836ea40bc72f64fbc87e768d
Paolo Paci <paolopci@yahoo.it>
2026-01-29 15:41:10 +0100

    fissa l'header

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
+import { LoadingService } from './core/services/loading.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   protected title = signal('Skinet');
+  private readonly loadingService = inject(LoadingService);
+  readonly loading$ = this.loadingService.loading$;
 }
