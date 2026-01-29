1768abd653e88c3ff75bb02b666c347f5fd0acf0
Paolo Paci <paolopci@yahoo.it>
2026-01-29 14:37:25 +0100

    not-found page complete , product non trovato!!!

--- client/src/app/shared/components/not-found/not-found.component.ts
+++ client/src/app/shared/components/not-found/not-found.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MATERIAL_IMPORTS } from '../../material';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [...MATERIAL_IMPORTS, RouterLink],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
