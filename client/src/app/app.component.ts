4a90732cab5e5f04fd91d80b4c45b973711d82f6
Paolo Paci <paolopci@yahoo.it>
2026-01-29 16:11:26 +0100

    add progress bar in shop.component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,35 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { LoadingService } from './core/services/loading.service';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { filter } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AsyncPipe],
+  imports: [RouterOutlet, HeaderComponent, AsyncPipe, MatProgressBar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   protected title = signal('Skinet');
   private readonly loadingService = inject(LoadingService);
+  private readonly router = inject(Router);
   readonly loading$ = this.loadingService.loading$;
+  private readonly currentUrl = signal(this.router.url);
+  readonly isShopRoute = computed(() => this.currentUrl().startsWith('/shop'));
+
+  constructor() {
+    this.router.events
+      .pipe(
+        filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+        takeUntilDestroyed(),
+      )
+      .subscribe((event) => {
+        this.currentUrl.set(event.urlAfterRedirects);
+      });
+  }
 }
