5399e0377f7917b1ba4219e544283948f39ed106
Paolo Paci <paolopci@yahoo.it>
2026-01-30 10:55:40 +0100

    create ShoppingCart Class for Redis

---
+++ Core/Entities/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class CartItem
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public string PictureUrl { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        public string Type { get; set; }
+    }
+}
---
+++ Core/Entities/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class ShoppingCart
+    {
+        [Required]
+        public string Id { get; set; }
+
+        public List<CartItem> Items { get; set; } = [];
+    }
+}
