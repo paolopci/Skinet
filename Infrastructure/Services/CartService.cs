d28f5adb0bc8a591d0dd9e123683d122d8d48bce
Paolo Paci <paolopci@yahoo.it>
2026-01-30 11:21:38 +0100

    create CartService e relativa Interface

---
+++ Infrastructure/Services/CartService.cs
@@ -0,0 +1,31 @@
+using Core.Entities;
+using Core.Interfaces;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Infrastructure.Services;
+
+public class CartService(IConnectionMultiplexer redis) : ICartService
+{
+    private readonly IDatabase _database = redis.GetDatabase();
+    public async Task<bool> DeleteCartAsync(string key)
+    {
+        return await _database.KeyDeleteAsync(key);
+    }
+
+    public async Task<ShoppingCart?> GetCartAsync(string key)
+    {
+        var data = await _database.StringGetAsync(key);
+        if (data.IsNullOrEmpty)
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<ShoppingCart>(data);
+    }
+
+    public Task<ShoppingCart> SetCartAsync(ShoppingCart cart)
+    {
+        throw new NotImplementedException();
+    }
+}
