2cce0bb30dff125bd3945f4c38c8f1369620f657
Paolo Paci <paolopci@yahoo.it>
2026-02-02 15:24:07 +0100

    add migration update database per register new fields

---
+++ Infrastructure/Migrations/20260202135846_AddUserAddress.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address_City",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_PostalCode",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_State",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Street",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address_City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Address_PostalCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Address_State",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Street",
+                table: "AspNetUsers");
+        }
+    }
+}
