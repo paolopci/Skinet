691cff90d68e062d2b5bee731d765f769f0884b4
Paolo Paci <paolopci@yahoo.it>
2026-02-02 17:38:09 +0100

    create Extensions

--- API/Program.cs
+++ API/Program.cs
@@ -1,122 +1,16 @@
-using Core.Interfaces;
-using Core.Entities;
-using Infrastructure.Data;
-using API.Middleware;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using StackExchange.Redis;
-using Infrastructure.Services;
-using System.Text;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<StoreContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddIdentityApiEndpoints<AppUser>(options =>
-    {
-        options.Password.RequireDigit = true;
-        options.Password.RequireLowercase = true;
-        options.Password.RequireUppercase = true;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequiredLength = 8;
-        options.User.RequireUniqueEmail = true;
-    })
-    .AddEntityFrameworkStores<StoreContext>();
-
-var jwtSection = builder.Configuration.GetSection("Jwt");
-var jwtKey = jwtSection["Key"];
-if (string.IsNullOrWhiteSpace(jwtKey))
-{
-    throw new InvalidOperationException("JWT key non configurata (Jwt:Key)");
-}
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidIssuer = jwtSection["Issuer"],
-            ValidAudience = jwtSection["Audience"],
-            ClockSkew = TimeSpan.FromMinutes(2)
-        };
-    });
-builder.Services.AddAuthorization();
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
-});
-
-// Add Redis
-var redisConnectionString = builder.Configuration.GetConnectionString("Redis") ??
-                            throw new Exception("Cannot get Redis connection string");
-
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = redisConnectionString;
-    options.InstanceName = "Skinet";
-});
-
-builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
-{
-    var configuration = ConfigurationOptions.Parse(redisConnectionString, true);
-
-    return ConnectionMultiplexer.Connect(configuration);
-});
-
-builder.Services.AddSingleton<ICartService, CartService>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddSingleton<IEmailSender, ConsoleEmailSender>();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddCorsPolicy(builder.Configuration);
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseApiPipeline();
 
-app.UseMiddleware<ExceptionMiddleware>();
-
-app.UseCors("CorsPolicy");
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-app.MapIdentityApi<AppUser>();
-try
-{
-    using var scope = app.Services.CreateScope();
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<StoreContext>();
-    await context.Database.MigrateAsync();
-    await StoreContextSeed.SeedAsync(context);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex);
-    throw;
-}
+await app.ApplyMigrationsAndSeedAsync();
 
 app.Run();
