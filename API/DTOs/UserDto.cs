70c04048166455d4747fd3383eeb99a39df1716d
Paolo Paci <paolopci@yahoo.it>
2026-02-02 11:29:41 +0100

    Create Dto for endpoints Identity (Request and Response)

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+namespace API.DTOs;
+
+public class UserDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+}
